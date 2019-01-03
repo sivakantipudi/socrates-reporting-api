8595ef1b5f77bf6ec82448c2e1b16734f878b1fa
sivaram <skantipudi@intraedge.com>
2019-01-03 11:28:42 +0530

    code changes

--- socrates-reporting-api/src/main/java/com/bec/socratesrpt/rest/model/TestDetails.java
+++ socrates-reporting-api/src/main/java/com/bec/socratesrpt/rest/model/TestDetails.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright Benchmark Education Company
+ *
+ * (C) Copyright Benchmark	All rights reserved.
+ *
+ * NOTICE:  All information contained herein or attendant here to is,
+ *          and remains, the property of Benchmark.  Many of the
+ *          intellectual and technical concepts contained herein are
+ *          proprietary to Benchmark. Any dissemination of this
+ *          information or reproduction of this material is strictly
+ *          forbidden unless prior written permission is obtained
+ *          from Benchmark.
+ *
+ * ------------------------------------------------------------------------
+ *
+ * ========================================================================
+ * Revision History
+ * ========================================================================
+ * DATE				: PROGRAMMER  : DESCRIPTION
+ * ========================================================================
+ * DEC 17 2018		: BEC       : CREATED.
+ * ------------------------------------------------------------------------
+ *
+ * ========================================================================
+ */
 package com.bec.socratesrpt.rest.model;
 
 public class TestDetails {
